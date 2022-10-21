a64002637b51ae02c663c0ca5d346f54dbc444e5
Clarkbar36 <alexclark36@gmail.com>
2022-10-21 16:28:15 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# will need to install packages:
-# pip install configparser
-
-# set api key in config file
-# import in traceback
-# file needs to be in directory as script
-# from EmailFunc import sendemail
-
